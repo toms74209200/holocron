3f5ec35390d3acbfc8827381093eb3b1dac38a88
toms74209200 <motomotomato@gmail.com>
2026-02-01 01:16:24 +0900

    :sparkles: Update DB schema for book

--- database/queries/book.sql
+++ database/queries/book.sql
@@ -2,6 +2,10 @@
 SELECT book_id, code, title, authors, publisher, published_date, thumbnail_url, occurred_at
 FROM book_events
 WHERE book_id = ? AND event_type = 'created'
+  AND NOT EXISTS (
+    SELECT 1 FROM book_events AS del
+    WHERE del.book_id = book_events.book_id AND del.event_type = 'deleted'
+  )
 LIMIT 1;
 
 -- name: CountBookByBookId :one
